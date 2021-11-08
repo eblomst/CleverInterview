6ccf40195d3ef462861b7610f7e7856e6973552e
eblomst <emma.blomsterberg@hotmail.com>
2021-11-08 23:33:36 +0100

    added unit test for create and authentication and test factory

---
+++ RFIDApp.Tests/RFIDFunctionTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Xunit;
+using RFIDApp.Functions;
+using RFIDApp.Models;
+using Newtonsoft.Json;
+using RFIDApp.Tests.Helpers;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace RFIDApp.Tests
+{
+    public class RFIDFunctionTests
+    {
+        private readonly ILogger logger = NullLoggerFactory.Instance.CreateLogger("Null Logger");
+
+        [Fact]
+        public async void Http_trigger_create_should_return_rfid_tagAsync()
+        {
+            var rfid = new RFIDTable("0E00010001");
+            var request = TestFactory.CreateHttpRequestWithBody(JsonConvert.SerializeObject(rfid));
+            var collector = new AsyncCollector<RFIDTable>();
+            OkObjectResult response = (OkObjectResult)await Create.Run(request, collector, logger);
+            Assert.Equal($"RFID tag id: {rfid.TagId} is saved to table storage", response.Value);
+        }
+
+
+        [Fact]
+        public async void Http_trigger_create_empty_tagid_should_return_bad_request_object_result()
+        {
+            var rfid = new RFIDTable(null);
+            var request = TestFactory.CreateHttpRequestWithBody(JsonConvert.SerializeObject(rfid));
+            var collector = new AsyncCollector<RFIDTable>();
+            BadRequestObjectResult response = (BadRequestObjectResult)await Create.Run(request, collector, logger);
+            Assert.Equal("TagId value is missing", response.Value);
+        }
+
+        [Fact]
+        public async void Http_trigger_create_null_body_result_should_return_bad_request_object_result()
+        {
+            
+            var request = TestFactory.CreateHttpRequestWithBody(null);
+            var collector = new AsyncCollector<RFIDTable>();
+            BadRequestObjectResult response = (BadRequestObjectResult)await Create.Run(request, collector, logger);
+            Assert.Equal("TagId value is missing", response.Value);
+        }
+
+        [Fact]
+        public async void Http_trigger_authentication_should_return_true()
+        {
+            var request = TestFactory.CreateHttpRequestWithQueryString("tagId", "0E00010001");
+            OkObjectResult response = (OkObjectResult)await Authentication.Run(request, new MockCloudTable(), logger);
+            Assert.Equal(true, response.Value);
+        }
+
+
+        [Fact]
+        public async void Http_trigger_authentication_should_return_false()
+        {
+            var request = TestFactory.CreateHttpRequestWithQueryString("tagId", "0E00010003");
+            OkObjectResult response = (OkObjectResult)await Authentication.Run(request, new MockCloudTable(), logger);
+            Assert.Equal(false, response.Value);
+        }
+    }
+}
---
+++ RFIDApp.Tests/TestFactory.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Primitives;
+using RFIDApp.Models;
+
+namespace RFIDApp.Tests
+{
+    public class TestFactory
+    {
+        public static IEnumerable<RFIDTable> Data()
+        {
+            return new List<RFIDTable>
+            {
+                new RFIDTable("0E00010001"),
+                new RFIDTable("0E00020002"),
+                new RFIDTable("0E00030003")
+
+            };
+        }
+
+        private static Dictionary<string, StringValues> CreateDictionary(string key, string value)
+        {
+            var qs = new Dictionary<string, StringValues>
+            {
+                { key, value }
+            };
+            return qs;
+        }
+
+        public static HttpRequest CreateHttpRequestWithQueryString(string queryStringKey, string queryStringValue)
+        {
+            var context = new DefaultHttpContext();
+            var request = context.Request;
+            request.Query = new QueryCollection(CreateDictionary(queryStringKey, queryStringValue));
+            return request;
+        }
+
+        public static HttpRequest CreateHttpRequestWithBody(string body)
+        {
+           
+            var context = new DefaultHttpContext();
+            var request = context.Request;
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            
+            writer.Write(body);
+            writer.Flush();
+            stream.Position = 0;
+             
+            request.Body = stream;
+
+            return request;
+        }
+
+    }
+}
