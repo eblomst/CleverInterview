f5a16895e595a8b40e78e14b0fc5199dbca59267
eblomst <emma.blomsterberg@hotmail.com>
2021-11-08 23:32:57 +0100

    added test helpers to mock async collector and cloud table

---
+++ RFIDApp.Tests/Helpers/AsyncCollector.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+
+namespace RFIDApp.Tests.Helpers
+{
+    public class AsyncCollector<T> : IAsyncCollector<T>
+    {
+        public readonly List<T> Items = new List<T>();
+
+        public Task AddAsync(T item, CancellationToken cancellationToken = default(CancellationToken))
+        {
+
+            Items.Add(item);
+
+            return Task.FromResult(true);
+        }
+
+        public Task FlushAsync(CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return Task.FromResult(true);
+        }
+    }
+}
---
+++ RFIDApp.Tests/Helpers/MockCloudTable.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos.Table;
+
+namespace RFIDApp.Tests.Helpers
+{
+    public class MockCloudTable : CloudTable
+    {
+
+        public MockCloudTable() : base(new Uri("http://127.0.0.1:10002/devstoreaccount1/RFID"))
+        {
+        }
+        public MockCloudTable(Uri tableAddress) : base(tableAddress)
+        { }
+
+        public MockCloudTable(StorageUri tableAddress, StorageCredentials credentials) : base(tableAddress, credentials)
+        { }
+
+        public MockCloudTable(Uri tableAbsoluteUri, StorageCredentials credentials) : base(tableAbsoluteUri, credentials)
+        { }
+
+        public async override Task<TableResult> ExecuteAsync(TableOperation operation)
+        {
+
+            var rowKey = operation
+                          .GetType()
+                          .GetProperties(BindingFlags.NonPublic | BindingFlags.Instance)
+                          .FirstOrDefault(prop => prop.Name == "RowKey")
+                          .GetValue(operation);
+
+            return await Task.FromResult(new TableResult
+            {
+                Result = TestFactory.Data().FirstOrDefault(x => x.RowKey.Equals(rowKey)),
+                HttpStatusCode = 200
+            });
+        }
+
+    }
+}
